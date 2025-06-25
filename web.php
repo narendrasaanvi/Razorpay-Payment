264cc30d24912237ac30b987d7851472ef50b635
Narendra Singh Kushwah <108320570+narendrasaanvi@users.noreply.github.com>
2025-06-25 21:04:17 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,1 +1,5 @@
 
+use App\Http\Controllers\PaymentController;
+Route::view('/admin/pay', 'admin/payment_form');
+Route::post('/payment/create-order', [PaymentController::class, 'createOrder']);
+Route::post('/payment/verify', [PaymentController::class, 'verifyPayment']);
